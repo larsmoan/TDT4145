31284ee082276af8b6e4f6ab4525cbd1692923db
Lars Moan <lars.ostberg.moan@gmail.com>
2023-03-20 19:26:53 +0100

    Begynte å starte å legge inn defaultinfo

--- main.py
+++ main.py
@@ -13,4 +13,18 @@ def create_db(filename):
 
     conn.close()
 
+def init_db(db_filename):
+    #Skal legge inn nødvendig data som ikke trenger å programmeres direkte
+    conn = sqlite3.connect(db_filename)
+    c = conn.cursor()
+
+    stasjons_query = """INSERT INTO Stasjon (navn, meterOverHavet) VALUES ('Trondheim', 5.1),
+    ('Steinkjer',3.6), ('Mosjøen',6.8), ('Mo i Rana', 3.5), ('Fauske', 34), ('Bodø', 4.1)"""
+    c.execute(stasjons_query)
+    conn.commit()
+    conn.close()
+
+create_db("sql_prosjektet.sql")
+init_db('sql_prosjektet.db')
+
 
